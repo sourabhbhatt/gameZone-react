a2bdf4256badcd9ff7c8e673e39109d26c93d24d
sourabh-dia <sourabh.bhatt@logicbuds.com>
2024-12-18 19:31:06 +0530

    winning animation

--- src/games/NehlePeDelha/WinningModal.js
+++ src/games/NehlePeDelha/WinningModal.js
@@ -1,23 +1,57 @@
 import React, { memo } from "react";
 import PropTypes from "prop-types";
 import Lottie from "lottie-react";
-import winningAnimation from "./assets/tictactoeSuccessAnimation.json";
+import winningAnimation from "./assets/nehlePeDehlaAnimation.json";
+import trophy from "./assets/Trophy.png";
+import { motion } from "framer-motion";
 
 const WinningModal = ({ isOpen, onPlayAgain, winnerName }) => {
   if (!isOpen) return null;
 
+  const trophyAnimation = {
+    hidden: { scale: 0, opacity: 0 },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 120,
+        damping: 8,
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-50 z-50">
       <div className="flex flex-col items-center justify-center p-8 w-[90%] max-w-md">
-        <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">
-          🎉 {winnerName} {winnerName === "You" ? "Won" : "Wins"}! 🎉
-        </h2>
-        <Lottie
-          animationData={winningAnimation}
-          loop
-          autoplay
-          className="w-48 h-48"
-        />
+        {winnerName === "You" ? (
+          <>
+            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">
+              🎉 "You Won"
+            </h2>
+            <motion.img
+              src={trophy}
+              alt="Trophy"
+              initial="hidden"
+              animate="visible"
+              variants={trophyAnimation}
+              className="w-48 h-48 object-contain"
+            />
+          </>
+        ) : (
+          <>
+            <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">
+              Bot Wins!
+            </h2>
+            <Lottie
+              animationData={winningAnimation}
+              loop
+              autoplay
+              className="w-48 h-48"
+            />
+          </>
+        )}
         <button
           onClick={onPlayAgain}
           className="mt-6 px-6 py-2 bg-purple-600 text-white text-lg font-semibold rounded-full hover:bg-purple-700 transition-all"
